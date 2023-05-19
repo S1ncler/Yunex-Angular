87ca13c10473c9ca3a7bfdc72ea1608daa2633b1
S1ncler <daniel_felipe_c11@hotmail.com>
2023-05-19 10:49:30 -0500

    Se sube proyecto con la estructura general

---
+++ src/app/horas/horas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HorasRoutingModule } from './horas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HorasRoutingModule
+  ]
+})
+export class HorasModule { }
---
+++ src/app/login-register/login-register.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRegisterRoutingModule } from './login-register-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoginRegisterRoutingModule
+  ]
+})
+export class LoginRegisterModule { }
